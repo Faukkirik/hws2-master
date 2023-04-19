fa426d9b36390bf1281fb381fe5b89566909d53f
Faukkirik <kote_33419@mail.ru>
2023-04-19 11:19:35 +0300

    3 commit - hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,33 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            console.log(state)
+            return action.payload === 'up' ? state.sort(function (a, b) {
+                if (a.name < b.name) {
+                    return -1;
+                }
+                if (a.name > b.name) {
+                    return 1;
+                }
+                return 0;
+            }) : state.sort(function (a, b) {
+                if (a.name < b.name) {
+                    return 1;
+                }
+                if (a.name > b.name) {
+                    return -1;
+                }
+                return 0;
+            })
+            // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            console.log(state)
+            return state.filter((el)=> el.age >= 18)
+            // need to fix
         }
         default:
             return state
