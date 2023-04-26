f11515e4f2a7158f94a5f8a204ed34da1d082636
Faukkirik <kote_33419@mail.ru>
2023-04-26 17:20:15 +0300

    8 commit - hw12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,15 @@
 const initState = {
     themeId: 1,
 }
-
-export const themeReducer = (state = initState, action: any): any => { // fix any
+type initStateType = typeof initState
+export const themeReducer = (state = initState, action: changeThemeIdActionType): initStateType => { // fix any
     switch (action.type) {
         // дописать
-
+        case 'SET_THEME_ID':
+            return {...state,themeId: action.id}
         default:
             return state
     }
 }
-
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type changeThemeIdActionType = ReturnType<typeof changeThemeId>
+export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id }) // fix any
