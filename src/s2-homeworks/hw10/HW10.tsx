d3aa341091a46827f82691402a03f6fd6692dceb
Faukkirik <kote_33419@mail.ru>
2023-04-26 14:15:36 +0300

    6 commit - hw10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -16,12 +16,12 @@ import {Loader} from './Loader'
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
     const dispatch = useDispatch()
-    const isLoading = useSelector<AppStoreType>((store)=>store.loading.isLoading)
+    const isLoading = useSelector<AppStoreType, boolean>((store)=>store.loading.isLoading)
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
         dispatch(loadingAC(!isLoading))
-        setTimeout(()=>{dispatch(loadingAC(!!isLoading));},1500)
+        setTimeout(()=>{dispatch(loadingAC(isLoading));},1500)
         // setTimeout
     }
 
